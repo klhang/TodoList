2349076b83e38aa95683e1da21831d8b03f1b3f7
Klhang <chrisjunhang@gmail.com>
2018-09-20 15:51:11 -0700

    update

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const root = document.getElementById('content');
+ReactDom.render(<h1>react will take over here!!!</h1>, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
+// webpack.config.js
 var path = require('path');
 
 module.exports = {
-  entry: "./frontend/todo_redux.jsx",
+  entry: './frontend//todo_redux.jsx',
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    filename: './bundle.js',
   },
   module: {
     rules: [
       {
-         test: /\.jsx?$/,
-         exclude: /(node_modules)/,
-         use: {
-           loader: 'babel-loader',
-           query: {
-             presets: ['env', 'react']
-           }
-         },
-       }
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
     ]
   },
-
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
