af59d6d4b64657e1483627873a60fc1c01c7f7e5
Klhang <chrisjunhang@gmail.com>
2018-09-21 17:02:01 -0700

    add selector for reducers

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import configureStore from './store/store';
-import {receiveTodos, receiveTodo} from './actions/todo_actions';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
+import App from './components/app';
+import Root from './components/root';
+import { allTodos } from './reducers/selectors';
 
 window.store = configureStore;
 window.receiveTodos = receiveTodos;
 window.receiveTodo = receiveTodo;
+window.allTodos = allTodos;
 
 const root = document.getElementById('content');
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDom.render(<h1>react will take over here!!!</h1>, root);
+  ReactDom.render(<Root />, root);
 })
